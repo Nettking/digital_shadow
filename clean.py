e61e4fb4411f5540eff80864e67709fd555b7ec1
Nettking <m@nettking.no>
2023-03-14 18:15:15 +0100

    Code and comment cleanup

--- clean.py
+++ clean.py
@@ -3,13 +3,10 @@ import pandas as pd
 # read CSV file
 data = pd.read_csv('data.csv')
 
-# drop the first column
+# drop unnecessary columns
 data = data.drop(data.columns[0], axis=1)
-
 data = data.drop(data.columns[1], axis=1)
-
 data = data.drop(data.columns[1], axis=1)
-
 data = data.drop(data.columns[2], axis=1)
 
 # save modified data to new CSV file
