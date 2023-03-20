125ef251ee29eb878a49daf571309abb58aa12a5
Nettking <m@nettking.no>
2023-03-20 12:13:14 +0100

    added switch signals

--- mqtt.py
+++ mqtt.py
@@ -1,18 +1,44 @@
 import paho.mqtt.client as mqtt
 
-# Function to establish a connection to the MQTT broker
-def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT,MQTT_TOPIC_SUB, MQTT_TOPIC_PUB, message = "Hello, world!"):
+switchState = False
+
+def on_message(client, userdata, message):
+    # Decode the message payload from bytes to string
+    payload = message.payload.decode()
+    global switchState
+    if payload == '{"SwitchOff":{"did":1}}':
+        
+        #print('Slår av...')
+        switchState = False
+    elif payload == '{"SwitchOn":{"did":1}}':
+        #print('Slår på...')
+        switchState = True
+
     
+
+# Function to establish a connection to the MQTT broker
+def establish_connection(MQTT_BROKER_ADDR, MQTT_BROKER_PORT, MQTT_TOPIC_SUB, MQTT_TOPIC_PUB, message="Hello, world!"):
+
     # Set up the MQTT client and connect to the broker
     client = mqtt.Client()
     client.connect(MQTT_BROKER_ADDR, MQTT_BROKER_PORT)
 
-    # Subscribe to the specified topic
-    client.subscribe(MQTT_TOPIC_SUB)
+    # Subscribe to the specified topic with QoS level 1
+    client.subscribe(MQTT_TOPIC_SUB, qos=1)
 
-    # Publish a message to the specified topic
-    client.publish(MQTT_TOPIC_PUB, message)
+    # Clear any retained messages on the subscribed topic
+    client.publish(MQTT_TOPIC_SUB, payload=None, retain=True)
+
+    # Publish a message to the specified topic with the retain flag set to False and QoS level 1
+    client.publish(MQTT_TOPIC_PUB, message, retain=False, qos=1)
     print("Published message: " + message)
+
+    # Set up the callback function for receiving messages
+    client.on_message = on_message
+
+    return client
+
+
 
 if __name__ == '__main__':
     host = '192.168.0.124'
--- shadow.py
+++ shadow.py
@@ -6,7 +6,6 @@ import time
 data = pd.read_csv('output.csv')
 
 # Convert time column to datetime object
-
 data['time'] = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S.%f')
 temp_list = []
 
@@ -15,8 +14,26 @@ topic = 'CPS2021/tempoutput'
 topic_response = 'CPS2021/SwitchControl'
 port = 1883
 
+# Establish a single connection to the MQTT broker
+client = establish_connection(host, port, topic_response, topic)
+last_temp = 20
+
+# Publish temperature data for each row in the CSV file
 for i, row in data.iterrows():
     current_temp = row['temp']
+    
+    if switchState == True:
+        print('On')    
+    elif switchState == False:
+        print('Off')
+    last_temp = current_temp
+
     message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{current_temp} }} }}'
-    establish_connection(host, port, topic, topic_response, message)
+    client.publish(topic, message, retain=False, qos=1)
+    print("Published message: " + message)
+
+    # Clear the message by publishing an empty payload with retain=True
+    client.publish(topic, payload=None, retain=True)
+
+    client.loop(timeout=1.0)
     time.sleep(1)
