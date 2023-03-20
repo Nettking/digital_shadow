7f9856dd4e2a7d7dbfece1772c4530637a3882da
benjydp <59916344+benjydp@users.noreply.github.com>
2023-03-20 12:34:45 +0100

    super epic beans

--- shadow.py
+++ shadow.py
@@ -16,24 +16,40 @@ port = 1883
 
 # Establish a single connection to the MQTT broker
 client = establish_connection(host, port, topic_response, topic)
-last_temp = 20
+last_temp = 20.5
+current_temp = 20.5
+mem = [20.5]
+
+def predict_temp(curr_temp, prev_temp, mem):
+
+    # Compute new temperature based on previous temperature and switch
+    if switchState == True:
+        new_temp = curr_temp + 0.1 * (20 - curr_temp) + 0.1 * (prev_temp - curr_temp)
+    else:
+        new_temp = curr_temp - 0.1 * (curr_temp - mem[-1]) + 0.1 * (prev_temp - curr_temp)
+
+    # Append new temperature to memory
+    mem.append(new_temp)
+
+    # Return list of predicted temperatures
+    return new_temp
 
 # Publish temperature data for each row in the CSV file
-for i, row in data.iterrows():
-    current_temp = row['temp']
+while(True):
     
-    if switchState == True:
-        print('On')    
-    elif switchState == False:
-        print('Off')
-    last_temp = current_temp
-
-    message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{current_temp} }} }}'
+    new_temp = predict_temp(current_temp, last_temp, mem)
+    
+    message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{new_temp} }} }}'
     client.publish(topic, message, retain=False, qos=1)
     print("Published message: " + message)
+
+    last_temp = current_temp
+
+    current_temp = new_temp
 
     # Clear the message by publishing an empty payload with retain=True
     client.publish(topic, payload=None, retain=True)
 
     client.loop(timeout=1.0)
     time.sleep(1)
+
