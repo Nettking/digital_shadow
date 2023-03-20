cc6c4cf8f987e9aba997f84652133ab7b8e54ec8
Nettking <m@nettking.no>
2023-03-20 12:34:16 +0100

    Update shadow.py

--- shadow.py
+++ shadow.py
@@ -20,18 +20,23 @@ last_temp = 20
 
 # Publish temperature data for each row in the CSV file
 for i, row in data.iterrows():
-    current_temp = row['temp']
+    curr_temp = row['temp']
     
-    if switchState == True:
+    if switchState:
         print('On')    
-    elif switchState == False:
+    else:
         print('Off')
-    last_temp = current_temp
-
-    message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{current_temp} }} }}'
+    
+    last_temp = curr_temp
+    
+    if switchState:
+        new_temp = curr_temp + 0.1 * (20 - curr_temp) + 0.1 * (last_temp - curr_temp)
+    else:
+        new_temp = curr_temp - 0.1 * (curr_temp - 20) + 0.1 * (last_temp - curr_temp)
+        
+    message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{new_temp} }} }}'
     client.publish(topic, message, retain=False, qos=1)
     print("Published message: " + message)
-
     # Clear the message by publishing an empty payload with retain=True
     client.publish(topic, payload=None, retain=True)
 
