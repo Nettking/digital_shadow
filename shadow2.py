ff3f096581eec3132c33cc36643c1858964893d0
Nettking <m@nettking.no>
2023-03-20 12:54:00 +0100

    Update shadow2.py

--- shadow2.py
+++ shadow2.py
@@ -2,14 +2,6 @@ import pandas as pd
 import time
 import numpy as np
 from mqtt import *
-from sklearn.linear_model import LinearRegression
-
-# Read the data
-data = pd.read_csv('data.csv')
-
-# Convert time column to datetime object
-data['time'] = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S:%f')
-temp_list = []
 
 host = '192.168.0.124'
 topic = 'CPS2021/tempoutput'
@@ -19,24 +11,22 @@ port = 1883
 # Establish a single connection to the MQTT broker
 client = establish_connection(host, port, topic_response, topic)
 
-# Prepare the data for the linear regression model
-data['switch'] = data['switch'].replace({'ON': 1, 'OFF': 0})
-data['switch'] = data['switch'].astype(int)
-X = data[['switch', 'temp']]
-y = data['temp']
-
-# Train the linear regression model
-model = LinearRegression()
-model.fit(X, y)
-
 # Initialize temperature and heating state
 current_temp = 20
 heating_state = False
 
+# Define temperature change parameters
+heating_rate = 0.025
+cooling_rate = 0.025
+time_interval = 1
+
 # Simulate the room temperature changes based on heating state
 while True:
     # Update the current_temp based on the heating_state
-    current_temp = model.predict([[int(heating_state), current_temp]])[0]
+    if heating_state:
+        current_temp += heating_rate * time_interval
+    else:
+        current_temp -= cooling_rate * time_interval
 
     # Publish the current temperature
     message = f'{{ "temperature": {{ "id": 1, "txt": "temperature", "t":{current_temp} }} }}'
